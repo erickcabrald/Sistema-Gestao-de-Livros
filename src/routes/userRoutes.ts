c59f2ba19205e88e398db2ff8b30b92b437aacb0
cabral <cabralerick39@gmail.com>
2024-10-19 22:34:05 -0300

    crinado a rota put

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -32,7 +32,9 @@ export function UserRoutes(app: FastifyInstance) {
 
     if (error) throw error;
 
-    return data;
+    return reply
+      .status(200)
+      .send({ message: 'Usuario criado com sucesso', data });
   });
 
   //Leitura de Usuarios
@@ -41,5 +43,46 @@ export function UserRoutes(app: FastifyInstance) {
 
     if (error) throw error;
     return data;
+  });
+  //atualizando usuarios
+  app.put('/user/:id', async (request, reply) => {
+    const { id } = request.params as { id: any };
+
+    const Schema = z.object({
+      name: z.string().min(4, { message: 'Name is required' }).optional(),
+      age: z
+        .number()
+        .min(12, { message: 'Age must be at least 12' })
+        .optional(),
+      password: z
+        .string()
+        .min(8, {
+          message: 'Password must be at least 8 characters',
+        })
+        .optional(),
+    });
+
+    const result = Schema.safeParse(request.body);
+
+    if (!result.success) {
+      return reply.status(400).send({ error: result.error.format() });
+    }
+
+    const { name, age, password } = result.data;
+
+    const { data, error } = await supabase
+      .from('users')
+      .update({
+        name: result.data.name,
+        age: result.data.age,
+        password: result.data.password,
+      })
+      .match({ id });
+
+    if (error) throw error;
+
+    return reply
+      .status(200)
+      .send({ message: 'Usuario atualizado com sucesso', data });
   });
 }
